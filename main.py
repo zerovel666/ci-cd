a12b047d7580af778fcae02bffcc44ddcfc94ddc
zerovel666 <zerovel060606@gmail.com>
2026-09-16 00:02:18 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+
+
+from datetime import datetime
+
+from fastapi import FastAPI
+
+
+app = FastAPI(title="Test")
+
+@app.get("/health-check")
+async def health_check():
+    date = datetime.now().date()
+    time = datetime.now().time()
+    return {
+        "status": True,
+        "timestamp": f"{date} {time}"
+    }
